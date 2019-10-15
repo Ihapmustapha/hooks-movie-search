10d4b94023dc3b557817c95817639e9f447b6bc2
ihapmustapha <ihapmustapha@ymail.com>
2019-10-15 15:57:01 +0200

    adding hooks to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useEffect } from "react";
+// styles
+import "./App.css";
+// components
+import Header from "./components/Header";
+import Movie from "./components/Movie";
+import Search from "./components/Search";
 
-function App() {
+const MOVIE_API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=37dc294e";
+
+const initialState = {
+  loading: true,
+  movies: [],
+  errorMessage: null
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SEARCH_MOVIE_REQUEST":
+      return {
+        ...state,
+        loading: true,
+        errorMessage: null
+      };
+    case "SEARCH_MOVIE_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: null,
+        movies: action.payload
+      };
+    case "SEARCH_MOVIE_FAIL":
+      return {
+        ...state,
+        loading: false,
+        errorMessage: action.error
+      };
+
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        dispatch({
+          type: "SEARCH_MOVIE_SUCCESS",
+          payload: jsonResponse.Search
+        });
+        console.log(state);
+      });
+  }, []);
+
+  const search = searchValue => {
+    dispatch({ type: "SEARCH_MOVIE_REQUEST" });
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=37dc294e`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          dispatch({
+            type: "SEARCH_MOVIE_SUCCESS",
+            payload: jsonResponse.Search
+          });
+        } else {
+          dispatch({ type: "SEARCH_MOVIE_FAIL", error: jsonResponse.Error });
+        }
+      });
+  };
+  const { loading, errorMessage, movies } = state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="Hooks Movie Search" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies!</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+          <span>loading...</span>
+        ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : movies ? (
+          movies.map((movie, index) => (
+            <Movie
+              key={`key-${movie.Title}-${movie.Year}`}
+              movie={movie}
+            ></Movie>
+          ))
+        ) : null}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
