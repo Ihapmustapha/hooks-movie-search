fbf7ec3a0b82b05479afb876344d39910b8b5ae4
ihapmustapha <ihapmustapha@ymail.com>
2019-10-15 15:57:20 +0200

    creating header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = props => {
+  return (
+    <header className="App-header">
+      <h3>{props.title}</h3>
+    </header>
+  );
+};
+
+export default Header;
