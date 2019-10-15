d9fdb856c267b1f17a7f0dc01c72e6c84d091f9e
ihapmustapha <ihapmustapha@ymail.com>
2019-10-15 15:57:38 +0200

    creating movie component

---
+++ src/components/Movie.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const DEFAULT_IMAGE_PLACEHOLDER =
+  "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+
+const Movie = ({ movie }) => {
+  const poster =
+    movie.poster === "N/A" ? DEFAULT_IMAGE_PLACEHOLDER : movie.Poster;
+
+  return (
+    <div className="movie">
+      <h2>{movie.Title}</h2>
+      <div>
+        <img width="200" src={poster} alt={`The movie titled ${movie.Title}`} />
+        <p>({movie.Year})</p>
+      </div>
+    </div>
+  );
+};
+
+export default Movie;
